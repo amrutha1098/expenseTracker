74d8d1c02a5fd2de6427efe26c0751fc228585b2
amrutha <amruthar1098@gmail.com>
2022-01-15 12:03:02 +0530

    added exceptions to track errors

--- database.py
+++ database.py
@@ -31,29 +31,29 @@ class MyDatabase:
         return data
 
     def insert_table_data_expense_tracker(self, data):
-        # cursor.execute(
-        #     'Insert into EXPENSE_TRACKER (UserId, addExpense, category, dateTime, description, payee) values (' +
-        #     str(data["userId"]) + ',' + str(data["addExpense"]) + ',' + str(data["category"]) + ',' + str(data["dateTime"]) + ',' + str(data[
-        #         "description"]) + ',' + data["payee"] + ';')
-        query = 'Insert into EXPENSE_TRACKER (UserId, addExpense, category, dateTime, description, payee) values (' + str(
-            data["userId"]) + ',' + str(data["addExpense"]) + ',"' + str(data["category"]) + '","' + str(
-            data["dateTime"]) + '","' + str(data[
-                                                "description"]) + '","' + data["payee"] + '");'
-        print(query)
-        self.cursor.execute(query)
-
-        self.connection.commit()
+        try:
+            query = 'Insert into EXPENSE_TRACKER (UserId, addExpense, category, dateTime, description, payee) values (' + str(
+                data["userId"]) + ',' + str(data["addExpense"]) + ',"' + str(data["category"]) + '","' + str(
+                data["dateTime"]) + '","' + str(data[
+                                                    "description"]) + '","' + data["payee"] + '");'
+            print(query)
+            self.cursor.execute(query)
+            self.connection.commit()
+        except Exception as error:
+            print("Error inserting into database {}".format(error))
+        else:
+            print("Inserted data succcessfully into DB ")
 
     def update_table_data_expense_tracker(self, data):
-        # cursor.execute(
-        #     'Insert into EXPENSE_TRACKER (UserId, addExpense, category, dateTime, description, payee) values (' +
-        #     str(data["userId"]) + ',' + str(data["addExpense"]) + ',' + str(data["category"]) + ',' + str(data["dateTime"]) + ',' + str(data[
-        #         "description"]) + ',' + data["payee"] + ';')
-        query = 'UPDATE EXPENSE_TRACKER set addExpense = ' + str(data["addExpense"]) + ', category = "' + str(
-            data["category"]) + '", dateTime = "' + str(
-            data["dateTime"]) + '", description = "' + str(data["description"]) + '" , payee = "' + data[
-                    "payee"] + '" WHERE userId = ' + str(data['userId']) + ';'
-        print(query)
-        self.cursor.execute(query)
-
-        self.connection.commit()
+        try:
+            query = 'UPDATE EXPENSE_TRACKER set addExpense = ' + str(data["addExpense"]) + ', category = "' + str(
+                data["category"]) + '", dateTime = "' + str(
+                data["dateTime"]) + '", description = "' + str(data["description"]) + '" , payee = "' + data[
+                        "payee"] + '" WHERE userId = ' + str(data['userId']) + ';'
+            print(query)
+            self.cursor.execute(query)
+            self.connection.commit()
+        except Exception as error:
+            print("Error updating into database {}".format(error))
+        else:
+            print("Updated data succcessfully into DB ")
--- expensetrackermain.py
+++ expensetrackermain.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from database import *
 from datetime import datetime
 
+
 class ExpenseTrackerAPI(MyDatabase):
 
     def __init__(self):
@@ -11,20 +12,32 @@ class ExpenseTrackerAPI(MyDatabase):
     def add_expense(self):
         @self.app.route('/addexpense', methods=['POST'])
         def add_expense():
-            data = request.get_json()
-            data["dateTime"] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-            print(data)
-            self.insert_table_data_expense_tracker(data)
-            return data
+            try :
+                data = request.get_json()
+                data["dateTime"] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                print(data)
+                self.insert_table_data_expense_tracker(data)
+                return data
+            except Exception as error:
+                print("Failed to do a post request {}".format(error))
+            else:
+                print("successful post request ")
 
     def update_expense(self):
         @self.app.route('/addexpense', methods=['PUT'])
         def update_expense():
-            data = request.get_json()
-            data["dateTime"] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-            print(data)
-            self.update_table_data_expense_tracker(data)
-            return data
+            try:
+                data = request.get_json()
+                data["dateTime"] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                print(data)
+                self.update_table_data_expense_tracker(data)
+                return data
+            except Exception as error:
+                print("Failed to do a put request {}".format(error))
+            else:
+                print("successful put request ")
+
+
 
 # Run Server
 if __name__ == '__main__':
